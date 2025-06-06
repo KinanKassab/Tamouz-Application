fb87e16343b543f13b377e4466a18718b6971238
KinanKassab <mayizaldeen@gmail.com>
2025-06-06 15:00:51 +0400

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,53 @@
 const CACHE_NAME = 'tamouz-cache-v1';
 const urlsToCache = [
-  '/',
-  '/download.html',
-  '/app.js',
-  '/manifest.json',
-  '/style.css',
-  '/index.html',
-  '/images/Logo.png',
-  '/images/icon.png',
-  '/Home.html'
+  '/Tamouz-Application/',
+  '/Tamouz-Application/download.html',
+  '/Tamouz-Application/app.js',
+  '/Tamouz-Application/manifest.json',
+  '/Tamouz-Application/style.css',
+  '/Tamouz-Application/index.html',
+  '/Tamouz-Application/images/Logo.png',
+  '/Tamouz-Application/images/icon.png',
+  '/Tamouz-Application/Home.html',
+  '/Tamouz-Application/offline.html'
 ];
 
+// تثبيت الكاش
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// تفعيل وتحديث الكاش
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('🧹 حذف الكاش القديم:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
+
+// التعامل مع الطلبات
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() =>
+          // في حال عدم وجود إنترنت
+          caches.match('/Tamouz-Application/offline.html')
+        )
+      );
+    })
   );
 });
