eb1cce0fe981b16c0be09c4b86858587881bdcaa
KinanKassab <165284464+KinanKassab@users.noreply.github.com>
2025-06-10 01:53:03 +0300

    Added package-lock.json

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  },
+  publicDir: 'public'
+});
